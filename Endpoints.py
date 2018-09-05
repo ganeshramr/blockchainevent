64746e290cca521d4c56a27472414be4e1908297
ganehsramr <mailganesh@gmail.com>
2018-09-05 14:33:23 -0400

    updates 3

--- Endpoints.py
+++ Endpoints.py
@@ -15,7 +15,30 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We will run the proof of work algorithm to get the next proof..
+    last_block = blockchain.last_block
+    proof = blockchain.proof_of_work(last_block)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender = "0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new block by adding it to the chain.
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New block forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods = ['POST'])
@@ -41,6 +64,11 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return jsonify(response), 200
+
+
+@app.route('/transactions/pending', methods=['GET'])
+def pending():
+    return jsonify(blockchain.currentTransactions),200
 
 
 if __name__ == '__main__':
